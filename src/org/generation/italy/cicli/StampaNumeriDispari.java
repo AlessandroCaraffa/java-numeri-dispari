626e7782891c00bc2c5555cb57413d2b06d172e0
AlessandroCaraffa <alessandro.caraffa93@gmail.com>
2021-11-18 17:22:37 +0100

    aggiunte liste numeri pari e dispari

--- src/org/generation/italy/cicli/StampaNumeriDispari.java
+++ src/org/generation/italy/cicli/StampaNumeriDispari.java
@@ -13,18 +13,28 @@ public class StampaNumeriDispari {
 		System.out.print("Quanto sarà lunga la lista?");
 		int n = Integer.parseInt(scanner1.nextLine()); 
 		int [] lista = new int[n];
+		int numeriDispari= 0 ;
+		int numeriPari = 0;
+		int sommaNumeri = 0;
+		
 
 		//creazione numeri
 		for(int i = 0; i < lista.length ; i ++) {
 			lista[i] = randomGenerator.nextInt(100);
 		
+			
+			
 		//stampa numeri	
 			if (lista[i] % 2 != 0 ) {
 				System.out.print(lista[i] + "  ");
+				numeriDispari += 1;
+			}else {
+				numeriPari += 1;
 			}
 			
 		}
-		
+		System.out.println("Il numero di numeri Dispari è " + numeriDispari);
+		System.out.println("Il numeri di numeri Pari è " + numeriPari);
 		
 		
 		
